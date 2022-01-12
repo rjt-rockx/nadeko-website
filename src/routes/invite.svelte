25fdefbe6bdc75464bf2497dfa0dbc3edcba018d
rjt <rjt.rockx@gmail.com>
2022-01-12 19:13:58 +0530

    Redirect invite

---
+++ src/routes/invite.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+	export async function load() {
+		return {
+			status: 301,
+			redirect:
+				'https://discord.com/oauth2/authorize?client_id=170254782546575360&scope=bot&permissions=8'
+		};
+	}
+</script>
