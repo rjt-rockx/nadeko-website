e072480d5d2090cebf27b1c21a34f05a5da4f6b8
rjt <rjt.rockx@gmail.com>
2022-01-22 22:30:55 +0530

    Add Lander sections

---
+++ src/components/Lander/LanderSection.svelte
@@ -0,0 +1,119 @@
+<script lang="ts">
+	export let title: string;
+	export let description: string | undefined;
+	export let imageSource: string | undefined = 'xp.png';
+	export let imageAlt: string | undefined = 'xp';
+	export let imageOnRight = true;
+	export let image: boolean = !!(imageSource && imageAlt);
+</script>
+
+<div class="container">
+	{#if image}
+		{#if imageOnRight}
+			<div class="section reverseWrap">
+				<div class="text part">
+					<h4 class="title">{title}</h4>
+					{#if description} <p class="description">{description}</p>{/if}
+				</div>
+				<div class="imageContainer">
+					<img class="image" src={imageSource} alt={imageAlt} />
+				</div>
+			</div>
+		{:else}
+			<div class="section">
+				<div class="imageContainer rightAlign">
+					<img class="image" src={imageSource} alt={imageAlt} />
+				</div>
+				<div class="text">
+					<h4 class="title">{title}</h4>
+					{#if description} <p class="description">{description}</p>{/if}
+				</div>
+			</div>
+		{/if}
+		<slot />
+	{:else}
+		<div class="section">
+			<div class="text center">
+				<h4 class="title">{title}</h4>
+				{#if description} <p class="description">{description}</p>{/if}
+			</div>
+		</div>
+		<slot />
+	{/if}
+</div>
+
+<style>
+	.container {
+		margin: 64px 0px;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.section {
+		max-width: min(75%, 125vh);
+		display: flex;
+		flex-flow: row wrap;
+		flex-grow: 1;
+		align-items: center;
+		gap: 36px;
+	}
+
+	.reverseWrap {
+		flex-flow: row wrap-reverse;
+	}
+
+	.text {
+		min-width: 256px;
+		max-width: 100%;
+		flex-grow: 1;
+		flex-basis: 0;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.center {
+		text-align: center;
+	}
+
+	.imageContainer {
+		min-width: 256px;
+		flex-grow: 1;
+		flex-basis: 0;
+		display: flex;
+	}
+
+	.rightAlign {
+		justify-content: flex-end;
+	}
+
+	.image {
+		width: 100%;
+		height: auto;
+		border-radius: 8px;
+	}
+
+	.title {
+		font-family: 'Source Sans Pro';
+		font-style: normal;
+		font-weight: 600;
+		font-size: 32px;
+		line-height: 40px;
+		max-width: 70ch;
+		margin: 0px;
+		color: #f2f6ff;
+	}
+
+	.description {
+		font-family: 'Source Sans Pro';
+		font-style: normal;
+		font-weight: normal;
+		font-size: 18px;
+		line-height: 24px;
+		max-width: 70ch;
+		margin-top: 16px;
+		color: rgba(242, 247, 255, 0.75);
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,21 @@
 <script>
 	import '../app.css';
 	import Topbar from '../components/Navigation/Topbar.svelte';
+	import Button from '../components/Button/Button.svelte';
 	import Footer from '../components/Footer/Footer.svelte';
 	import Hero from '../components/Lander/Hero.svelte';
+	import LanderSection from '../components/Lander/LanderSection.svelte';
+	import { onMount, afterUpdate } from 'svelte';
+
+	const renderTwemoji = () => {
+		// @ts-ignore
+		twemoji.parse(
+			document.body,
+			{ folder: 'svg', ext: '.svg' } // This is to specify to Twemoji to use SVGs and not PNGs
+		);
+	};
+	onMount(renderTwemoji);
+	afterUpdate(renderTwemoji);
 
 	const navigationTargets = [
 		{
@@ -23,6 +36,8 @@
 			url: '/support'
 		}
 	];
+
+	const landerSections = [{}];
 
 	const footerSections = [
 		{
@@ -87,10 +102,48 @@
 	];
 </script>
 
+<svelte:head>
+	<title>NadekoBot</title>
+</svelte:head>
+
 <div class="container">
 	<Topbar {navigationTargets} />
 	<div class="sections">
 		<Hero />
+		<LanderSection
+			title="A command for everything."
+			description="Moderation, games, leveling, it's all built into Nadeko. Set up a one-stop shop for your server's roles or manage your server's members efficiently with a single bot that does it all."
+			image={true}
+			imageSource="modules.png"
+			imageAlt="A list of Nadeko's modules"
+			imageOnRight={true}
+		/>
+		<LanderSection
+			title="Customizable XP/level-up system"
+			description="Having a customizable XP system with clubs, leaderboards, level up rewards and more, Nadeko lets you modify a user's XP and even assign roles/award currency on level up!"
+			image={true}
+			imageSource="xp.png"
+			imageAlt="A Nadeko XP card"
+			imageOnRight={false}
+		/>
+		<LanderSection
+			title="One of a kind currency economy"
+			description="Gamble your flowers, claim waifus, participate in events and put your currency at stake to have Nadeko decide your fate!"
+			image={true}
+			imageSource="currencyeconomy.png"
+			imageAlt="A leaderboard of Nadeko's users"
+			imageOnRight={true}
+		/>
+		<LanderSection
+			title="100% Open Source"
+			description="We’re an open source project, and we rely on your donations to support the 13+ million people using Nadeko. Donate to help pay for server costs and support the bot while also getting currency rewards in return!"
+			image={false}
+		>
+			<div class="buttonGroup">
+				<Button href="/patreon">Patreon</Button>
+				<Button href="/paypal">PayPal</Button>
+			</div>
+		</LanderSection>
 	</div>
 	<Footer sections={footerSections} />
 </div>
@@ -110,5 +163,13 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+	}
+
+	.buttonGroup {
+		display: flex;
+		flex-direction: row;
+		max-width: min(384px, calc(100% - 128px));
+		width: 100%;
+		gap: 24px;
 	}
 </style>
